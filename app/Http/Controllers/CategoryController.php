45280667625e419a1f6ad751a0f8c049f96923a9
dikiafriadi2004 <diki.afriadi2004@gmail.com>
2024-05-06 16:58:04 +0700

    update fiture

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -24,8 +24,32 @@ class CategoryController extends Controller
             'name' => 'required|unique:categories'
         ]);
 
-        $category = Category::create([
-            'name' => $request->name
+        $category =  new Category();
+        $category->name = $request->name;
+        $category->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => "Category successfully created",
+            'data' => $category
+        ], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|unique:categories'
         ]);
+
+        $category = Category::find($id);
+        $category->name = $request->name;
+        $category->update();
+
+        return response()->json([
+            'success' => true,
+            'message' => "Category successfully updated",
+            'data' => $category
+        ], 200);
+
     }
 }
